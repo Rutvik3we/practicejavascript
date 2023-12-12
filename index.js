42c2fec29c9afd0b27e00e152af637bcbf8b4827
Rutvik3we <you@example.com>
2023-12-12 14:45:18 +0530

    arrayobject

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
-let myObj = {
-    0: "name",
-    1: "rutvik"
-}
-myObj.name = "crc";
-//myObj.[2] = 23;
+// let myObj = {
+//     0: "name",
+//     1: "rutvik"
+// }
+// myObj.name = "crc";
+// //myObj.[2] = 23;
 
-console.log(myObj)
+// console.log(myObj)
+
+
+let arr = [];
+
+for (let i = 0; i<10; i++){
+    arr[i] = document.write("mak <br>")
+}
