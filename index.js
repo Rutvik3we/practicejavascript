627a2c7410b94846959ad4e92f6dd99c4e2c6ff9
Rutvik3we <you@example.com>
2023-12-12 15:05:27 +0530

    function

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+//1.object
+
 // let myObj = {
 //     0: "name",
 //     1: "rutvik"
@@ -8,8 +10,36 @@
 // console.log(myObj)
 
 
-let arr = [];
+//2.array
+// let arr = [];
 
-for (let i = 0; i<10; i++){
-    arr[i] = document.write("mak <br>")
-}
+// for (let i = 0; i<10; i++){
+//     arr[i] = document.write("mak <br>")
+// }
+
+
+//3.function
+// function add(){
+//     let result = ( 23*10 + 23*10 + 23*10) / 10;
+
+//     result = result *2*10;
+//     document.write(result);
+    
+//     result = result/20;
+//     document.write(result);
+// }
+
+// add();
+
+// function add(a,b,c){
+//     let result = ( a*10 + b*10 + c*10) / 10;
+
+//     // result = result *2*10;
+//     // document.write(result);
+    
+//     // result = result/20;
+//      document.write(result);
+// }
+
+// add(23,23,23);
+// add(45,45,45)
